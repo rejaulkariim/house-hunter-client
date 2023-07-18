ea7aaffd481aa8c2ab1b60a1011e37da4c02b3ad
karimscoding <rejalkarim66666@gmail.com>
2023-07-19 07:30:43 +0800

    implemented custom registration and redirect to dashboard based on role

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,45 @@
+import axios from "axios";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
 import { Link } from "react-router-dom";
 
 function LoginForm() {
+  const initialState = {
+    email: "",
+    password: "",
+  };
+
+  const [formData, setFormData] = useState(initialState);
+  console.log(formData)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/login",
+        formData
+      );
+      const token = response.data.token;
+      // Store the JWT token in local storage
+      localStorage.setItem("jwtToken", token);
+
+      toast.success("Login successful!");
+      // Clear the form data
+      setFormData(initialState);
+    } catch (error) {
+      console.log(error.response.data.message);
+      toast.error(error.response.data.message);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="flex items-center justify-center h-[600px]">
       <div className="w-[400px]">
@@ -8,16 +47,22 @@ function LoginForm() {
           <h1 className="text-xl font-bold ">Login to your account</h1>
           <p>Please fill out the form to create an account</p>
         </div>
-        <form className="flex flex-col gap-4 mt-4">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-4">
           <input
             type="email"
             placeholder="Email"
             className="py-2 px-4 rounded-md"
+            required
+    
+            onChange={handleInputChange}
           />
           <input
             type="password"
             placeholder="Password"
             className="py-2 px-4 rounded-md"
+            required
+        
+            onChange={handleInputChange}
           />
           <button type="submit" className="py-2 px-4 bg-accent rounded-md">
             Login
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -2,31 +2,46 @@ import axios from "axios";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { FaSpinner } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function RegisterForm() {
   const initialState = {
     fullName: "",
-    role: "House Owner",
+    role: "",
     phoneNumber: "",
     email: "",
     password: "",
   };
 
+  const navigate = useNavigate()
+
   const [formData, setFormData] = useState(initialState);
+  const [errorMessage, setErrorMessage] = useState("");
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
-    setLoading(true);
     e.preventDefault();
     try {
-      setFormData(initialState);
+      setLoading(true); // Start the loading spinner
       await axios.post("http://localhost:5000/api/register", formData);
+      setLoading(false); // Stop the loading spinner
       toast.success("Registration successful!");
-      setLoading(false);
+      setFormData(initialState);
+
+      // redirect user to dashboard based on their role
+      if(formData.role === "House Owner"){
+        navigate("/owner-dashboard")
+      }else{
+        navigate("/renter-dashboard")
+      }
     } catch (error) {
-      console.log(error.message.data.response);
-      toast.error(error.message.data.response);
+      setLoading(false); // Stop the loading spinner
+      if (error.response?.status === 409) {
+        setErrorMessage("Email already registered. Please login");
+      } else {
+        console.log(error.message.data.response);
+        setErrorMessage("Registration failed. Please try again later.");
+      }
     }
   };
 
@@ -45,6 +60,7 @@ function RegisterForm() {
           <h1 className="text-xl font-bold ">Register an account</h1>
           <p>Please fill out the form to create an account</p>
         </div>
+        {errorMessage && <p className="text-center text-error">{errorMessage}</p>}
         <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-4">
           <input
             type="text"
@@ -62,9 +78,14 @@ function RegisterForm() {
             className="py-2 px-4 rounded-md"
             required
           >
+            {" "}
+            <option value="" disabled selected>
+              Select Role
+            </option>
             <option value="House Owner">House Owner</option>
             <option value="House Renter">House Renter</option>
           </select>
+
           <input
             type="tel"
             name="phoneNumber"
@@ -92,9 +113,11 @@ function RegisterForm() {
             className="py-2 px-4 rounded-md"
             required
           />
-          <button type="submit" className="py-2 px-4 bg-accent rounded-md items-center">
-            {loading ? <FaSpinner className="animate-spin" /> : null}
-            Register
+          <button
+            type="submit"
+            className="py-2 px-4 bg-primary rounded-md"
+          >
+            {loading ? <FaSpinner className="animate-spin" /> : "Register"} 
           </button>
           <p className="text-center">
             {" "}
